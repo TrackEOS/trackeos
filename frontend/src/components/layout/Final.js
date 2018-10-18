fe34a4351d2beacff530eecb3fdeddcf64e2b02f
I am The Cheese <ch33s3codes@gmail.com>
2018-10-18 05:27:58 +0300

    Minor Cleanup

--- frontend/src/components/layout/Final.js
+++ frontend/src/components/layout/Final.js
@@ -108,30 +108,3 @@ class Final extends Component {
 }
 
 export default Final;
-
-
-
-
-
-// <tbody>
-//       {myList.map((item, i) => {
-//         return (
-//           <Fragment>
-//             <tr key={i} onClick={toggleMobileOpen.bind(this, i)}>
-//               <td className="toggler">
-//                 {item.mobile_open && <ArrowUp />}
-//                 {!item.mobile_open && <ArrowDown />}
-//               </td>
-//               <td>{item.elem_one}</td>
-//               <td>{item.elem_two}</td>
-//               <td>{item.elem_three}</td>
-//             </tr>
-//             {item.mobile_open &&
-//               <tr className="test-td">
-//                 <td>...</td>
-//               </tr>
-//             }
-//           </Fragment>
-//         );
-//       })}
-//     </tbody>
